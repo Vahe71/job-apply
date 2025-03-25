b5fa0ce3ea17baa54da294cd5d89f9db1cd7f83f
Vahe71 <v.h777a.m@gmail.com>
2025-03-25 14:44:39 +0400

    changes

--- src/components/CustomDatePicker.tsx
+++ src/components/CustomDatePicker.tsx
@@ -3,16 +3,25 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css"; // Убери, если будешь стилизовать вручную
 import { getYear, getMonth } from "date-fns";
 import { MiniCalendarIcon } from "../../public/icons/MiniCalendarIcon";
+import { GreenArrow } from "../../public/icons/GreenArrow";
+import { BlackArrow } from "../../public/icons/BlackArrow";
 
 interface DatePickerProps {
-  changeMilestoneData: (key: string, id:number, newData: any) => void; 
+  changeMilestoneData: (key: string, id: number, newData: any) => void;
   milestoneId: number;
   milestoneDate: Date | null;
 }
 
-const CustomDatePicker: React.FC<DatePickerProps> = ({changeMilestoneData, milestoneId, milestoneDate}) => {
+const CustomDatePicker: React.FC<DatePickerProps> = ({
+  changeMilestoneData,
+  milestoneId,
+  milestoneDate,
+}) => {
   const [dueDate, setDueDate] = useState<Date | null>(null);
-  const years = Array.from({ length: (2051 - 2025) }, (_, i) => getYear(new Date()) + i);
+  const years = Array.from(
+    { length: 2051 - 2025 },
+    (_, i) => getYear(new Date()) + i
+  );
   const months = [
     "January",
     "February",
@@ -38,17 +47,17 @@ const CustomDatePicker: React.FC<DatePickerProps> = ({changeMilestoneData, miles
         selected={milestoneDate}
         onChange={(date) => {
           if (date) {
-            setDueDate(new Date(date)); 
-            changeMilestoneData('dueDate', milestoneId, new Date(date))
+            setDueDate(new Date(date));
+            changeMilestoneData("dueDate", milestoneId, new Date(date));
           } else {
-            setDueDate(null); 
+            setDueDate(null);
           }
         }}
         dateFormat="dd.MM.yyyy"
-        className="focus:outline-none outline-none flex-grow text-[#8B939F] text-[16px] font-medium block w-full"
-        calendarClassName="bg-white border border-gray-200 rounded-lg p-2"
+        className="focus:outline-none outline-none flex-grow text-[#8B939F] text-[16px] font-medium block my-[12px] w-full"
+        calendarClassName="bg-white border border-gray-200 !rounded-[12px] p-2 pt-[18px] w-[350px] min-h-[350px] !flex flex-col items-center shadow-[2px_2px_5px_0px_rgba(0,0,0,0.1)]"
         dayClassName={(date) =>
-          "text-gray-900 hover:bg-blue-500 hover:text-white rounded-md w-10 h-10 flex items-center justify-center"
+          "max-w-[25px] h-[25px] !m-0 text-[13px] hover:!bg-[#F0F1F4] rounded-[4px] hover:text-red-200   flex items-center justify-center"
         }
         todayButton="Today"
         renderCustomHeader={({
@@ -60,51 +69,61 @@ const CustomDatePicker: React.FC<DatePickerProps> = ({changeMilestoneData, miles
           prevMonthButtonDisabled,
           nextMonthButtonDisabled,
         }) => (
-          <div className="flex justify-between items-center bg-gray-100 p-2 rounded-t-lg">
+          <div className="flex justify-between items-center p-2 pt-0 pb-[10px] rounded-t-lg">
             <button
               onClick={decreaseMonth}
               disabled={prevMonthButtonDisabled}
-              className="px-2 py-1 text-gray-600 hover:bg-gray-300 rounded-md"
+              className="mr-[3px] cursor-pointer"
             >
-              ◀
+              <GreenArrow />
             </button>
 
             <div className="flex items-center space-x-2">
-              <select
-                value={getYear(date)}
-                onChange={({ target: { value } }) =>
-                  changeYear(parseInt(value))
-                }
-                className="border rounded-md px-2 py-1"
-              >
-                {years.map((year) => (
-                  <option key={year} value={year}>
-                    {year}
-                  </option>
-                ))}
-              </select>
+              <div className="relative">
+                <select
+                  value={getYear(date)}
+                  onChange={({ target: { value } }) =>
+                    changeYear(parseInt(value))
+                  }
+                  className="date-year-dropdown outline-none border-[#AEB3BC] border-[1px] rounded-[12px] h-[42px] w-[93px] appearance-none p-[9px_8px] text-[#8B939F] text-[16px]"
+                >
+                  {years.map((year) => (
+                    <option className="" key={year} value={year}>
+                      {year}
+                    </option>
+                  ))}
+                </select>
+                <div className="absolute right-2 top-1/2 transform -translate-y-1/2 pointer-events-none">
+                  <BlackArrow />
+                </div>
+              </div>
 
-              <select
-                value={months[getMonth(date)]}
-                onChange={({ target: { value } }) =>
-                  changeMonth(months.indexOf(value))
-                }
-                className="border rounded-md px-2 py-1"
-              >
-                {months.map((month, index) => (
-                  <option key={index} value={month}>
-                    {month}
-                  </option>
-                ))}
-              </select>
+              <div className="relative">
+                <select
+                  value={months[getMonth(date)]}
+                  onChange={({ target: { value } }) =>
+                    changeMonth(months.indexOf(value))
+                  }
+                   className="outline-none border-[#AEB3BC] border-[1px] rounded-[12px] h-[42px] w-[125px] appearance-none p-[9px_8px] text-[#8B939F] text-[16px]"
+                >
+                  {months.map((month, index) => (
+                    <option key={index} value={month}>
+                      {month}
+                    </option>
+                  ))}
+                </select>
+                <div className="absolute right-2 top-1/2 transform -translate-y-1/2 pointer-events-none">
+                  <BlackArrow />
+                </div>
+              </div>
             </div>
 
             <button
               onClick={increaseMonth}
               disabled={nextMonthButtonDisabled}
-              className="px-2 py-1 text-gray-600 hover:bg-gray-300 rounded-md"
+              className="ml-[3px] rotate-180 cursor-pointer"
             >
-              ▶
+              <GreenArrow />
             </button>
           </div>
         )}
